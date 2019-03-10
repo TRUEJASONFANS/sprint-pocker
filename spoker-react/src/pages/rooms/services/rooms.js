c443509b20ffc8ad0fb4f2d691a35cf73b86e086
truejasonfans <472408570@qq.com>
2019-03-11 01:25:30 +0800

    Integration with websocket room query with spring sequrity.

--- spoker-react/src/pages/rooms/services/rooms.js
+++ spoker-react/src/pages/rooms/services/rooms.js
@@ -1,14 +1,30 @@
 import { PAGE_SIZE } from '../constants';
-import {request} from '../../../utils/request';
+// import {request} from '../../../utils/request';
+import {openSocket, request} from '../../../services/websocket_service';
 
-export function fetch({ page = 1 }) {
-  // ` `字符串模版
-  return request(`/api/rooms=${page}&_limit=${PAGE_SIZE}`);
-}
+// export function fetch({ page = 1 }) {
+//   // ` `字符串模版
+//   return request(`/api/rooms=${page}&_limit=${PAGE_SIZE}`);
+// }
 
-export function create(values) {
-  request('/api/addRoom',
-    // JSON.stringify(values)
+// export function create(values) {
+//   request('/api/addRoom',
+//     // JSON.stringify(values)
+//     {}
+//   );
+// }
+
+export function create(values) {	
+  request('/app/addRoom', {},	 
+    JSON.stringify(values),	 
     {}
   );
+}
+
+export function w_connect(action) {
+  openSocket('/pocker/rooms', action, w_request);
+}
+
+export function w_request() {
+  request('/app/rooms', {}, {});
 }
