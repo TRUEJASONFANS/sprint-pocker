99af72c49b768fcbc4372ff409d729b85160da29
Jason Xie <zhong-wei.xie@asml.com>
2019-03-12 18:36:55 +0800

    dashboard UI init, model and service.

--- spoker-react/src/pages/dashboard/model/dashboardModel.js
+++ spoker-react/src/pages/dashboard/model/dashboardModel.js
@@ -1,21 +1,31 @@
 import * as dashboardService from '../service/dashboardService';
+
 export default {
   namespace: 'dashboard',
   state: {
-    
+    itemList: [],
+    page: 1
+
   },
   reducers: {
-
+    save(state, { payload: { data: list, page } }) {
+      
+      return { ...state, list, page };
+    },
   },
   effects: {
-    *queryPokerNotes() {
-      
+    *fetch({ payload: page }, { call }) {
+      yield call(dashboardService.fetch, page);
     },
-    *deletePokerNote() {
-
+    *create({ payload: newItem }, { call, select ,put}) {
+      yield call(dashboardService.create, newItem);
+      const page = yield select(state => state.users.page);
+      yield put({ type: 'fetch', payload: { page } });
     },
-    *addPokerNote() {
-    
-    },
+    *deleteOne({ payload: ticketNum }, { call, select, put }) {
+      yield call(dashboardService.deleteOne, ticketNum);
+      const page = yield select(state => state.users.page);
+      yield put({ type: 'fetch', payload: { page } });
+    }
   }
 }
--- spoker-react/src/pages/dashboard/service/dashboardService.js
+++ spoker-react/src/pages/dashboard/service/dashboardService.js
@@ -1,5 +1,25 @@
 import * as request from '../../../utils/request';
-function queryPokerNotes(ticketRecord) {
-  request("/poker/ticketRecord", ticketRecord);
+import { PAGE_SIZE } from '../../constants';
+
+export function fetch(page) {
+  return request(`/api/dashboard?page=${page}&_limit=${PAGE_SIZE}`, {
+    method:'get'
+  });
 }
 
+
+export function create(newItem) {
+  return request(`/api/dashboard`, {
+    method:'post',
+    body: JSON.stringify(newItem),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
+
+export function deleteOne(ticketNum) {
+  return request(`/api/dashboard/${ticketNum}`, {
+    method:'delete',
+  });
+}
