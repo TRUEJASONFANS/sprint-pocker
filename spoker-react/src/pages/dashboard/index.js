2bc85dd4ceb55d98be73da0be85bdbefab946a89
Jason Xie <zhong-wei.xie@asml.com>
2019-03-12 18:36:55 +0800

    dashboard UI init, model and service.

--- spoker-react/src/pages/dashboard/index.js
+++ spoker-react/src/pages/dashboard/index.js
@@ -1,9 +1,10 @@
 import styles from './dashboard.css';
+import Dashoboard from './components/dashboard.js';
 
 export default function() {
   return (
     <div className={styles.normal}>
-      <h1>Page poker_dashboard</h1>
+      <Dashoboard/>
     </div>
   );
 }
