d3261f40d3252f7a8cb6818db38264bdd7578000
zhxie <zhong-wei.xie@asml.com>
2019-06-11 18:57:53 +0800

    add the jest unit test samples

---
+++ spoker-react/src/__test__/helloworld.test.js
@@ -0,0 +1,7 @@
+import { mount } from 'enzyme';
+import TestDemo from '../component/TestDemo'
+
+test('TestDemo', () => {
+  const wrapper = mount(<TestDemo />);
+  expect(wrapper.find('div').text()).toBe('test');
+});
