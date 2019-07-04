81c3322a1478f9a6d1f7e0814f46d0552efc91d8
truejasonfans <472408570@qq.com>
2019-07-05 00:19:49 +0800

    card

--- spoker-react/src/pages/pockerRoom/components/pokerBoardFooter.tsx
+++ spoker-react/src/pages/pockerRoom/components/pokerBoardFooter.tsx
@@ -1,16 +1,13 @@
 import * as React from 'react';
+import styles from './pokerBoardFooter.css';
 
 export interface Props {
   cards: [];
 }
 
-function PokerBoardFooter({cards}: Props) {
+export default function PokerBoardFooter({cards}: Props) {
  
-  for (let card of cards) {
-    <div className="hand hhand-compact active-hand">
-          <img className='card' src='cards/AS.svg'/>
-    </div>
-  }
+
   return (
     <div>
         {
@@ -23,6 +20,5 @@ function PokerBoardFooter({cards}: Props) {
   );
 }
 
-export default PokerBoardFooter;
 
 // helpers
