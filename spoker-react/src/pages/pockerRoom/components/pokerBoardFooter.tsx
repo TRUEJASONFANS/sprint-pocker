1de4779d81b6917d89abc265851a018682304812
truejasonfans <472408570@qq.com>
2019-07-08 23:55:07 +0800

    abstract card component

--- spoker-react/src/pages/pockerRoom/components/pokerBoardFooter.tsx
+++ spoker-react/src/pages/pockerRoom/components/pokerBoardFooter.tsx
@@ -1,20 +1,34 @@
 import * as React from 'react';
-<<<<<<< HEAD
 import styles from './card.css';
-=======
-import styles from './pokerBoardFooter.css';
->>>>>>> a9b666a3f2e115a40bcad630e7f8b09cf302590d
 
 export interface Props {
-  cards: [];
+  cards: string[];
 }
 
-<<<<<<< HEAD
 export interface State {
-  flag: true;
+  flag: boolean;
+}
+
+interface Props2 {
+  index: number;
+  card: string;
 }
 
 class PokerBoardFooter extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className={styles.playerCardsContainer}>
+        {this.props.cards.map((card, index) => (<Card card={card} index={index} key={index} />))}
+      </div>
+    );
+  }
+
+}
+
+class Card extends React.Component<Props2, State> {
   constructor(props) {
     super(props);
 
@@ -22,60 +36,44 @@ class PokerBoardFooter extends React.Component<Props, State> {
     this.state = {
       flag: true,
     };
-    this.render0= this.render0.bind(this);
+    this.onClickHandler = this.onClickHandler.bind(this);
+  }
+  onClickHandler() {
+    let curFlag = this.state.flag;
+    this.setState({
+      flag: !curFlag
+    });
   }
   render() {
-    return (
-      <div className={styles.playerCardsContainer}>
-        {this.props.cards.map((card, index) => (this.render0(card, index, this.state.flag)))}
-      </div>
-    );
-  }
-  render0(card: string, index: number, nowFlag: boolean) {
-    let leftStyle = { left: `${20 + index * 70}px` };
-    function onClickHandler() {
-      this.setState({ flag: !nowFlag });
-      console.log(nowFlag);
-    }
-    
+    let leftStyle = { left: `${20 + this.props.index * 70}px` };
     const containStyle = {
       width: 100,
       height: 140,
-      transform: this.state.flag ? "transform: matrix3d(0.968846, -0.247665, 0, 0, 0.246399, 0.963893, 0.100983, 0, -0.0250101, -0.0978374, 0.994888, 0, 0, -126.512, 0, 1)" :
-        "transform: matrix(1, 0, 0, 1, 0, 0)",
+      transform: this.state.flag ? "matrix(1, 0, 0, 1, 0, 0)" :
+        "matrix3d(0.968846, -0.247665, 0, 0, 0.246399, 0.963893, 0.100983, 0, -0.0250101, -0.0978374, 0.994888, 0, 0, -126.512, 0, 1)"
     };
-
     return (
       <div className={`${styles.cardRig} ${styles.cardInHand}`} style={leftStyle}>
         <div className={`${styles.cardWrapper} ${styles.perspectiveWrapper}`}>
           <div className={styles.animationWrapper}>
-            <div className={`${styles.cardContainer}`} onClick={onClickHandler.bind(this)}>
+            <div className={`${styles.cardContainer}`} onClick={this.onClickHandler} style={containStyle}>
               <div className={`${styles.card} ${styles.cardBack}`} />
               <div className={` ${styles.cardFace}`}>
                 <div className={styles.smallCardId}>
-                  <span>{card}</span>
+                  <span>{this.props.card}</span>
                 </div>
                 <div className={styles.playerVote}>
-                  <span>{card}</span>
+                  <span>{this.props.card}</span>
                 </div>
               </div>
-=======
-export default function PokerBoardFooter({cards}: Props) {
- 
-
-  return (
-    <div>
-        {
-          cards.map(card => (
-            <div className="hand hhand-compact active-hand">
-              <img className='card' src='cards/AS.svg'/>
->>>>>>> a9b666a3f2e115a40bcad630e7f8b09cf302590d
             </div>
           </div>
         </div>
       </div>);
+
   }
 }
 
+export default PokerBoardFooter;
 
 // helpers
