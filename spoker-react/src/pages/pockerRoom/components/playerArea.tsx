2c0bdc69f00b47e3df2f9ccf5d550bb782536ba4
truejasonfans <472408570@qq.com>
2019-07-21 17:57:35 +0800

    init poker player area

---
+++ spoker-react/src/pages/pockerRoom/components/playerArea.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+class PlayerSelectedCard extends React.Component {
+
+  render() {
+    return <div/>
+  }
+}
+
+export default class PlayerArea extends React.Component {
+
+
+  render() {
+    return <div/>
+  }
+}
+
