e21c3931aa2b48b4b9a294741aa3f5fd7fce159a
zhxie <zhong-wei.xie@asml.com>
2019-11-25 17:47:44 +0800

    add card into candidate board

--- spoker-react/src/pages/pockerRoom/components/candidateBoard.tsx
+++ spoker-react/src/pages/pockerRoom/components/candidateBoard.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Modal } from 'antd';
+import styles from './candidateBoard.css';
 interface Card {
   clicked: boolean,
   shown: boolean,
@@ -11,15 +12,15 @@ interface Props {
   resetHandler: Function,
 }
 interface State {
-  visiable : boolean,
+  visiable: boolean,
 }
-export default class CandidateBoard extends React.PureComponent<Props,State> {
-  timeHanlder : NodeJS.Timeout
+export default class CandidateBoard extends React.PureComponent<Props, State> {
+  timeHanlder: NodeJS.Timeout
 
   constructor(props) {
     super(props);
     this.state = {
-      visiable : false,
+      visiable: false,
     }
     this.cancel = this.cancel.bind(this);
   }
@@ -27,31 +28,31 @@ export default class CandidateBoard extends React.PureComponent<Props,State> {
   componentDidUpdate = () => {
     let shown = false;
     this.props.usersList.forEach(card => {
-        if (card.shown) {
-          shown = true;
-        }
+      if (card.shown) {
+        shown = true;
+      }
     });
     if (shown && !this.state.visiable) {
-      this.timeHanlder = setTimeout(()=> {
+      this.timeHanlder = setTimeout(() => {
         this.setState({
-          visiable : true
+          visiable: true
         });
       }, 2000)
     }
   };
-  
+
   componentWillUnmount = () => {
     clearTimeout(this.timeHanlder);
   };
 
   cancel() {
-    this.setState({visiable: false});
+    this.setState({ visiable: false });
     console.log(this.props.resetHandler);
     this.props.resetHandler();
   }
   render() {
     let candidateCards = new Set();
-    this.props.usersList.map((card, index)=> {
+    this.props.usersList.map((card, index) => {
       candidateCards.add(card.fibonacciNum);
     });
     return (
@@ -62,7 +63,18 @@ export default class CandidateBoard extends React.PureComponent<Props,State> {
           footer={null}
           onCancel={this.cancel}
         >
-        {candidateCards.forEach(value=>{console.log(value)})}
+          {candidateCards.forEach(value => { 
+            <div className={`${styles.card}`} style={{ background: '#149c37' }}>
+            <div className={`${styles.cardContainer}`}>
+              <div className={styles.smallCardId}>
+                <span className={styles.smallCardIdSpan}>{value}</span>
+              </div>
+              <div className={styles.playerVote}>
+                <span>{value}</span>
+              </div>
+            </div>
+            </div>
+          })}
         </Modal>
       </div>
     )
