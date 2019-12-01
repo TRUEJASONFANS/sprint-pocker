f3d4de9aa6a2c0e5d042bf79713cdc527abfe315
truejasonfans <472408570@qq.com>
2019-12-02 00:35:16 +0800

    final candidat data

--- spoker-react/src/pages/pockerRoom/components/candidateBoard.tsx
+++ spoker-react/src/pages/pockerRoom/components/candidateBoard.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { Modal } from 'antd';
 import styles from './candidateBoard.css';
-import {connect} from 'dva';
-
+import { connect } from 'dva';
+import { FinalCandidate } from '../services/pockerService';
 interface Card {
   clicked: boolean,
   shown: boolean,
@@ -10,13 +10,14 @@ interface Card {
   playerName: string,
 }
 interface Props {
-  pockerBoard, 
-  loading
+  pockerBoard,
+  loading,
+  dispatch: Function,
 }
 interface State {
   visiable: boolean,
 }
-@connect(({ pockerBoard, loading }) => ({pockerBoard,loading}))
+@connect(({ pockerBoard, loading }) => ({ pockerBoard, loading }))
 export default class CandidateBoard extends React.PureComponent<Props, State> {
   timeHanlder: NodeJS.Timeout
   constructor(props) {
@@ -25,24 +26,20 @@ export default class CandidateBoard extends React.PureComponent<Props, State> {
       visiable: false,
     }
     this.cancel = this.cancel.bind(this);
+    this.onSelelctCandidate = this.onSelelctCandidate.bind(this);
   }
 
   componentDidUpdate = () => {
-    const { pockerBoard, loading } = this.props
-    const {scoreList, isOwner} = pockerBoard;
-    let shown = false;
-    scoreList.forEach(card => {
-      if (card.shown) {
-        shown = true;
-      }
-    });
-    if (shown && !this.state.visiable) {
-      this.timeHanlder = setTimeout(() => {
-        this.setState({
-          visiable: true
-        });
-      }, 2000)
-    }
+    const { pockerBoard } = this.props
+
+    // if (shown && !this.state.visiable) {
+    //   this.timeHanlder = setTimeout(() => {
+    //     this.setState({
+    //       visiable: true
+    //     });
+    //   }, 2000)
+    // }
+
   };
 
   componentWillUnmount = () => {
@@ -50,50 +47,101 @@ export default class CandidateBoard extends React.PureComponent<Props, State> {
   };
 
   cancel() {
+    const { pockerBoard, dispatch } = this.props
+    const { curPage, totalPage, roomName } = pockerBoard;
+    let values = {
+      curPage: curPage,
+      totalPage: totalPage,
+      roomName: roomName
+    }
+    dispatch({
+      type: "pockerBoard/onNextGame",
+      payload: values,
+    });
     this.setState({ visiable: false });
-    // this.props.resetHandler();
   }
+
+  onSelelctCandidate() {
+    this.setState({
+      visiable: true
+    });
+  }
+
   render() {
-    const { pockerBoard, loading } = this.props
-    const {scoreList, isOwner} = pockerBoard;
+    const { pockerBoard } = this.props
+    const { scoreList, isOwner } = pockerBoard;
+
     let candidateCardsSet = new Set();
     scoreList.map((card, index) => {
       candidateCardsSet.add(card.fibonacciNum);
     });
     let candidateCards = [...candidateCardsSet];
-    let wrapClassNameStyle = isOwner ? "candidateModalOwner":"candidateModal";
-
+    let wrapClassNameStyle = isOwner ? "candidateModalOwner" : "candidateModal";
+    let shown = false;
+    scoreList.forEach(card => {
+      if (card.shown) {
+        shown = true;
+      }
+    });
     return (
       <div>
         <Modal
           title="Candidate Cards"
-          visible={this.state.visiable}
+          visible={shown}
           footer={null}
           onCancel={this.cancel}
           maskClosable={false}
           wrapClassName={wrapClassNameStyle}
         >
           <div>
-            {candidateCards.map((card, index) => <PureCandidateCard card={card} key={index} isOwner={isOwner} />)}
+            {candidateCards.map((card, index) => (
+              <PureCandidateCard
+                card={card}
+                key={index}
+                isOwner={isOwner}
+                pockerBoard={pockerBoard}
+                dispatch={this.props.dispatch}
+                onSelelctCandidate={this.onSelelctCandidate}
+              />
+            ))}
           </div>
         </Modal>
       </div>
-    )
+    );
   }
 }
 interface CardProps {
   card: string,
-  isOwner: boolean
-
+  isOwner: boolean,
+  dispatch: Function,
+  pockerBoard: any,
+  onSelelctCandidate: Function
 }
 class PureCandidateCard extends React.PureComponent<CardProps> {
+  dipatch: Function
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+    this.dipatch = this.props.dispatch;
+  }
 
   onClick(e) {
     if (!this.props.isOwner) {
       e.preventDefault();
       return;
     }
+    const { curPage, roomName } = this.props.pockerBoard;
+    let finalCandidate:FinalCandidate = {
+      pageNum: curPage,
+      roomName: roomName,
+      score: this.props.card,
+    }
     // trigger the click final score broad cast
+    this.dipatch({
+      type: "pockerBoard/OnSelectCandidate",
+      payload: finalCandidate
+    });
+    this.props.onSelelctCandidate();
   }
   render() {
     return (
