02736507c37f6380ed76857be6750d14675c5fcc
truejasonfans <472408570@qq.com>
2019-12-22 23:31:12 +0800

    add the border for owner's card

--- spoker-react/src/pages/pockerRoom/components/playerAreaView.tsx
+++ spoker-react/src/pages/pockerRoom/components/playerAreaView.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import styles from './playerAreaView.css';
 import CandidateBoard from './candidateBoard';
+import { connect } from 'dva';
 interface Props {
-  usersList: Card[]
+  usersList: Card[],
+  global,
 }
 interface Card {
   clicked: boolean,
@@ -12,8 +14,10 @@ interface Card {
 }
 interface Props2 {
   card:Card,
-  index: number
+  index: number,
+  userName: string,
 }
+
 class PlayerSelectedCard extends React.PureComponent<Props2, any> {
   constructor(props) {
     super(props);
@@ -22,7 +26,7 @@ class PlayerSelectedCard extends React.PureComponent<Props2, any> {
     console.log("PayerCard updated");
     const card = this.props.card;
     let pokerCard;
-    let leftStyle = { left: `${10 + this.props.index * 110}px` };
+    let leftStyle = { left: `${10 + this.props.index * 110}px`, border : this.props.card.playerName === this.props.userName ? "SkyBlue dotted": ""};
     if (!card.shown) {// 如果还不能翻牌， 意思是指有人已经出牌，但并没有全部出完
       pokerCard = <div><div className={`${styles.card} ${styles.cardBack}`} /><div style={{font:"14px Georgia, serif"}}>{this.props.card.playerName}</div></div>;
       if (card.clicked) { // 该玩家已经出牌，但是碍于场上玩家并没有全部出牌，不翻牌。
@@ -30,7 +34,7 @@ class PlayerSelectedCard extends React.PureComponent<Props2, any> {
       }
     } else { // 全部出完了，可以显示牌了
       pokerCard = (
-        <div className={`${styles.card}`} style={{ background: '#149c37' }}>
+        <div className={`${styles.card}`} style={{ background: '#149c37'}}>
             <div className={`${styles.cardContainer}`}>
               <div className={styles.smallCardId}>
                 <span className={styles.smallCardIdSpan}>{this.props.card.fibonacciNum}</span>
@@ -48,14 +52,16 @@ class PlayerSelectedCard extends React.PureComponent<Props2, any> {
       </div>)
   }
 }
-
+@connect(({global})=> ({global}))
 export default class PlayerAreaView extends React.PureComponent<Props, any> {
 
   render() {
+    const {global} = this.props;
+    const {userName} = global;
     console.log("playerAreaView: rerender");
     return (
       <>
-        {this.props.usersList.map((card, index) => <PlayerSelectedCard key={index} index={index} card={card} />)}
+        {this.props.usersList.map((card, index) => <PlayerSelectedCard key={index} index={index} card={card} userName={userName}/>)}
         <CandidateBoard/>
       </>
      )
