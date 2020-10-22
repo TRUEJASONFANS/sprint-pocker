4ed47bf0cd489e979d1a52481cd72fc1f9bf896d
zhxie <zhong-wei.xie@asml.com>
2020-10-22 12:58:31 +0800

    remove the hard code url

--- spoker-server/web/src/main/resources/static/scripts/login.js
+++ spoker-server/web/src/main/resources/static/scripts/login.js
@@ -30,7 +30,7 @@ function signin() {
     var submit = function (callback) {
         console.log("提交注册表单!")
         let request = new XMLHttpRequest()
-        let url = "http://localhost:8080/user/regist";
+        let url = "/user/regist";
         request.open("POST", url, true)
         request.setRequestHeader('Content-type','application/json; charset=utf-8');
         let formData = {};
