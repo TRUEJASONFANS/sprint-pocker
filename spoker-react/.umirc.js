e54b7bb9e73d8371fd44bb17c66d2afc9bf6ad30
Jason Xie <zhong-wei.xie@asml.com>
2023-05-30 23:10:46 +0800

    add the file back

---
+++ spoker-react/.umirc.js
@@ -0,0 +1,31 @@
+
+// ref: https://umijs.org/config/
+
+const hostUrl = 'http://localhost:8111';
+
+export default {
+  outputPath: 'build',
+  treeShaking: true,
+  base:"/sprint",
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: true,
+      title: 'spoker-react',
+      dll: true,
+      routes: {
+        exclude: [],
+      },
+      hardSource: false,
+    }],
+  ],
+  "proxy": {// 配置fetch 路由转发至后台API服务地址.
+    "/sprint": {
+      "target": `${hostUrl}`,
+      "changeOrigin": true,
+      secure: false, // 不进行证书验证
+    },
+  },
+}
